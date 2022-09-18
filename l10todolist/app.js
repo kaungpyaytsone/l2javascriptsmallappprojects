7106231e01b278619ca1a38e8871e8fbf92820a0
kaungpyaytsone <kaungpyaytsone@gmail.com>
2022-09-18 17:28:55 +0630

    l10 todolist complete

--- l10todolist/app.js
+++ l10todolist/app.js
@@ -9,12 +9,70 @@ getform.addEventListener('submit',function(e){
     e.preventDefault();
 });
 
-function addnew(){
+function addnew(todo){
     var todotext = gettextbox.value;
     // console.log(todotext);
 
-    const li = document.createElement('li');
-    li.textContent = todotext;
-    
-    console.log(li);
+    if(todo){
+        todotext = todo.text;
+    }
+
+    if(todotext){
+        const li = document.createElement('li');
+
+        if(todo && todo.done){
+            li.classList.add('done');
+        }
+
+        li.textContent = todotext;
+        // console.log(li);
+
+        getul.appendChild(li);
+        gettextbox.value = '';
+        
+        updatelocalstorage();
+
+        // left click
+        li.addEventListener('click',function(){
+            li.classList.toggle('done');
+            updatelocalstorage();
+        });
+
+        //right click
+        li.addEventListener('contextmenu',function(){
+            li.remove();
+            updatelocalstorage();
+        });
+    }    
 }
+
+function updatelocalstorage(){
+    var getalllis = document.querySelectorAll('li');
+    // console.log(getalllis);
+
+    const todos = [];
+
+    getalllis.forEach(function(getallli){
+        todos.push({
+            text:getallli.textContent,
+            done:getallli.classList.contains('done')
+        });
+    }); 
+    
+    // console.log(todos);
+
+    localStorage.setItem('todos',JSON.stringify(todos));   
+}
+
+var getlstodos = JSON.parse(localStorage.getItem('todos'));
+// console.log(getlstodos);
+
+if(getlstodos){
+    getlstodos.forEach(function(getlstodo){
+        // console.log(getlstodo);
+        // console.log(getlstodo.text);
+
+        addnew(getlstodo);
+    });
+}
+
